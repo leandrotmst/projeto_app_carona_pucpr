82ed09488e925e4fb82726ce9da36716bbdb2ddb
leandro <leandrotomasetto@gmail.com>
2025-11-04 08:29:28 -0300

    estilizados/todo

---
+++ js/admin.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.getItem("sessao")){
+        var sessao = JSON.parse(localStorage.getItem("sessao"));
+        var pagina = "Seja bem vindo " + sessao.usuario;
+        document.getElementById("retorno").innerHTML = pagina;
+    }else{
+        window.location.href = "../index.html";
+    }
+});
